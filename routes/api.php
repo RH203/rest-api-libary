30e05fa11547cf08096a3684e66272cbb2814758
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2025-01-25 14:47:46 +0700

    ✨ feat(routes/api.php): add admin routes for genre retrieval and user deletion with authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\App\AdminController;
 use App\Http\Controllers\App\AuthController;
+use Illuminate\Support\Facades\Route;
 
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+
+Route::prefix('admin')->middleware(['auth:sanctum', 'abilities:admin'])->group(function () {
+  Route::get('/get-genre', [AdminController::class, 'getGenre']);
+  Route::post('/delete-user', [AdminController::class, 'deleteUser']);
+});
