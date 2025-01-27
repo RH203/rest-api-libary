613f6ae3a4d460402a87c90d92ebb7544435fb00
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2025-01-27 17:20:36 +0700

    ✨ feat(app/Models/Peminjaman.php): add SoftDeletes trait for Peminjaman model to enable soft deleting functionality

--- app/Models/Peminjaman.php
+++ app/Models/Peminjaman.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Peminjaman extends Model
 {
+
+  use SoftDeletes;
   protected $fillable = ['borrow_date', 'return_date', 'notes', 'student_id', 'book_id'];
 
   public function studentProfile(): BelongsTo
