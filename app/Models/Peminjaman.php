612537544b51b709c8dd1096fb1505f92e73e71b
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2025-01-27 15:19:35 +0700

    🚀 feat(models): add fillable properties to Peminjaman model for better mass assignment handling

--- app/Models/Peminjaman.php
+++ app/Models/Peminjaman.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Peminjaman extends Model
 {
+  protected $fillable = ['borrow_date', 'return_date', 'notes', 'student_id', 'book_id'];
+
   public function studentProfile(): BelongsTo
   {
     return $this->belongsTo(StudentProfile::class);
