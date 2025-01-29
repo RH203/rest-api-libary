700b0a9aee67f5c66b5339b9a01dbbd8dbc19586
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2025-01-29 15:55:41 +0700

    🚀 feat(StudentController.php): improve book borrowing logic and enhance error handling for book return process

--- app/Http/Controllers/App/StudentController.php
+++ app/Http/Controllers/App/StudentController.php
@@ -29,7 +29,7 @@ class StudentController extends BaseController
         'notes.string' => 'Catatan harus berupa string'
       ]);
 
-      $data = Peminjaman::where('book_id', $validate['book_id'])->where('student_id', $validate['student_id'])->first();
+      $data = Peminjaman::where('book_id', $validate['book_id'])->where('student_id', $validate['student_id'])->where('return_date', null)->first();
 
       if ($data) {
         return $this->error('Satu id cuma bisa meminjam satu buku yang sama.', 400);
@@ -41,11 +41,14 @@ class StudentController extends BaseController
         return $this->error('Buku tidak ditemukan', 404);
       }
 
-      if ($book->stock <= 0) {
-        return $this->error('Out of Stock', 404);
-      }
 
-      DB::transaction(function () use ($validate, $book) {
+      DB::transaction(function () use ($validate) {
+        $book = Book::where('id', $validate['book_id'])->lockForUpdate()->first();
+
+        if ($book->stock <= 0) {
+          throw new \ErrorException('Stok buku habis dari custom', 400);
+        }
+
         Peminjaman::create([
           'borrow_date' => now(),
           'book_id' => $validate['book_id'],
@@ -53,16 +56,61 @@ class StudentController extends BaseController
           'notes' => $validate['notes']
         ]);
 
-        $book->stock = $book->stock - 1;
+        $book->decrement('stock');
         $book->save();
       });
 
       return $this->success('Buku berhasil dipinjam');
     } catch (\Throwable $th) {
-      return $this->error('Terjadi kesalahan, silakan coba lagi', 500);
+      return $this->error($th->getMessage(), 500);
     }
   }
 
+  /**
+   * Mengembalikan buku
+   */
+  public function returnBook(Request $request)
+  {
+    try {
+      $validated = $request->validate([
+        'book_id' => 'required|numeric',
+        'student_id' => 'required|numeric'
+      ], [
+        'book_id.required' => 'ID buku tidak boleh kosong',
+        'book_id.numeric' => 'ID buku harus berupa angka',
+        'student_id.required' => 'ID siswa tidak boleh kosong',
+        'student_id.numeric' => 'ID siswa harus berupa angka'
+      ]);
+
+      $peminjaman = Peminjaman::where('book_id', $validated['book_id'])
+        ->where('student_id', $validated['student_id'])
+        ->whereNull('return_date')
+        ->first();
+
+      if (! $peminjaman) {
+        return $this->error('Peminjaman tidak ditemukan', 404);
+      }
+
+      $updateStock = Book::find($validated['book_id']);
+      if (!$updateStock) {
+        return $this->error('Buku tidak ditemukan', 404);
+      }
+
+      DB::transaction(function () use ($validated, $peminjaman) {
+        $updateStock = Book::where('id',  $validated['book_id'])->lockForUpdate()->first();
+        $updateStock->increment('stock');
+
+        $peminjaman->return_date = now();
+
+        $updateStock->save();
+        $peminjaman->save();
+      });
+
+      return $this->success('Buku berhasil dikembalikan');
+    } catch (\Throwable $th) {
+      return $this->error($th->getMessage(), 500);
+    }
+  }
 
   /**
    * Melihat semua buku
@@ -70,11 +118,13 @@ class StudentController extends BaseController
   public function getBook()
   {
     try {
-      $book = Book::with(['genre.name', 'publisher.name'])->get();
+      $book = Book::with(['publisher:id,name', 'genre:id,name'])
+        ->select('id', 'image', 'title', 'description', 'author', 'isbn', 'stock')
+        ->paginate(10);
 
-      return $this->success($book);
+      return $this->successPaginate($book);
     } catch (\Throwable $th) {
-      return $this->error('Failed fetch book', 500);
+      return $this->error($th->getMessage(), 500);
     }
   }
 
@@ -92,63 +142,14 @@ class StudentController extends BaseController
       ]);
 
       $book = Book::with([
-        'peminjaman' => [
-          'borrow_date',
-          'return_date',
-        ],
-        'peminjaman.studentProfile.full_name',
-        'genre.name',
-        'publisher.name'
-      ])->findOrFail($validate['book_id']);
+        'peminjaman',
+        'genre:id,name',
+        'publisher:id,name'
+      ])->select('id', 'image', 'title', 'description', 'author', 'isbn', 'stock')->findOrFail($validate['book_id']);
 
       return $this->success($book);
     } catch (\Throwable $th) {
-      return $this->error('Book not found', 500);
-    }
-  }
-
-  /**
-   * Mengembalikan buku
-   */
-  public function returnBook(Request $request) {
-    try {
-      $validated = $request->validate([
-        'book_id' => 'required|numeric',
-        'student_id' => 'required|numeric'
-      ], [
-        'book_id.required' => 'ID buku tidak boleh kosong',
-        'book_id.numeric' => 'ID buku harus berupa angka',
-        'student_id.required' => 'ID siswa tidak boleh kosong',
-        'student_id.numeric' => 'ID siswa harus berupa angka'
-      ]);
-
-      $peminjaman = Peminjaman::where('book_id', $validated['book_id'])
-        ->where('student_id', $validated['student_id'])
-        ->whereNull('return_date')
-        ->first();
-
-        if(! $peminjaman) {
-          return $this->error('Peminjaman tidak ditemukan', 404);
-        }
-
-        $updateStock = Book::find($validated['book_id']);
-        if (!$updateStock) {
-            return $this->error('Buku tidak ditemukan', 404);
-        }
-
-        DB::transaction(function () use ($peminjaman, $validated, $updateStock) {
-          $updateStock->stock = $updateStock->stock + 1;
-
-          $peminjaman->return_date = now();
-
-          $updateStock->save();
-          $peminjaman->save();
-
-        });
-
-        return $this->success('Buku berhasil dikembalikan');
-    } catch (\Throwable $th) {
-      return $this->error('Failed return book', 500);
+      return $this->error($th->getMessage(), 500);
     }
   }
 }
