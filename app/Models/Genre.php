0b388d8a6b0e5b6ab0614b0633264ff814ff74f5
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2025-01-29 15:55:30 +0700

    ✨ refactor(Genre.php): update book relation to specify pivot table and key names, improve date casting

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -9,11 +9,16 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Genre extends Model
 {
   use SoftDeletes;
-  protected $fillable = ['name'];
 
+  
+  protected $fillable = ['name'];
+  protected $casts = [
+    'created_at' => 'datetime',
+    'updated_at'=> 'datetime',
+  ];
 
   public function book(): BelongsToMany
   {
-    return $this->belongsToMany(Book::class);
+    return $this->belongsToMany(Book::class, 'genre_book', 'genre_id', 'book_id');
   }
 }
